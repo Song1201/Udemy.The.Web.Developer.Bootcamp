c1b5a4c27c77591d2c2445aa5363774b1b72a619
Song1201 <songchenli1201@outlook.com>
2019-01-08 16:58:03 -0800

    Added Patatap.Clone

--- To.Do.List/assets/js/todos.js
+++ To.Do.List/assets/js/todos.js
@@ -1,8 +1,22 @@
-$("li").click(function(){
+$("ul").on("click","li",function(){
   $(this).toggleClass("completed");
 });
 
-$("span").click(function(event){
-  $(this).parent().fade;
+$("ul").on("click","span",function(event){
+  $(this).parent().fadeOut(function() {
+    $(this).remove();
+  });
   event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+    var todoText = $(this).val();
+    $("ul").append("<li><span><i class='far fa-trash-alt'></i></span> " + todoText + "</li>");
+    $(this).val("");
+  }
+});
+
+$(".fa-plus").click(function() {
+  $("input[type='text'").fadeToggle();
 });
